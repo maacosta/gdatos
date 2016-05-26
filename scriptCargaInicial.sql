5ab32622edbb01329047329339b332eec2095640
maxiqutn <maqarg@fibertel.com.ar>
2016-05-25 22:43:35 -0300

    Create scriptCargaInicial.sql
    select de clientes y empresas

---
+++ scriptCargaInicial.sql
@@ -0,0 +1,7 @@
+use GD1C2016
+
+--clientes (personas fisicas)
+select  distinct Publ_Cli_Dni  ,publ_cli_nombre,Publ_Cli_Apeliido,Publ_Cli_Fecha_Nac,Publ_Cli_Dom_Calle ,Publ_Cli_Nro_Calle,Publ_Cli_Piso,Publ_Cli_Depto,Publ_Cli_Cod_Postal,Publ_Cli_Mail from gd_esquema.Maestra where Cli_Dni is not null and Publ_Cli_Nombre is not null order by Publ_Cli_Dni
+
+--empresas
+select distinct Publ_Empresa_cuit,Publ_Empresa_Razon_Social,Publ_Empresa_Dom_Calle ,Publ_Empresa_Nro_Calle ,Publ_Empresa_Piso,Publ_Empresa_Depto,Publ_Empresa_Cod_Postal,Publ_Empresa_Mail,Publ_Empresa_Fecha_Creacion from gd_esquema.Maestra where Publ_Empresa_cuit is not null order by Publ_Empresa_cuit
