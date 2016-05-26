874e16ce47865dc01fde8946970e7a3cc9d04cc6
maxiqutn <maqarg@fibertel.com.ar>
2016-05-25 23:11:59 -0300

    se agregan visibilidades (categorias)

--- scriptCargaInicial.sql
+++ scriptCargaInicial.sql
@@ -5,3 +5,6 @@ select  distinct Publ_Cli_Dni  ,publ_cli_nombre,Publ_Cli_Apeliido,Publ_Cli_Fecha
 
 --empresas
 select distinct Publ_Empresa_cuit,Publ_Empresa_Razon_Social,Publ_Empresa_Dom_Calle ,Publ_Empresa_Nro_Calle ,Publ_Empresa_Piso,Publ_Empresa_Depto,Publ_Empresa_Cod_Postal,Publ_Empresa_Mail,Publ_Empresa_Fecha_Creacion from gd_esquema.Maestra where Publ_Empresa_cuit is not null order by Publ_Empresa_cuit
+
+--visibilidades (gratis,plata,oro,bronce,platino)
+select distinct Publicacion_Visibilidad_Cod,Publicacion_Visibilidad_desc from gd_esquema.Maestra  where Publicacion_Visibilidad_Cod is not null
