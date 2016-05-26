1e616d5bd76f29498ada495469e7b21c126a2184
maxiqutn <maqarg@fibertel.com.ar>
2016-05-25 23:18:07 -0300

    se agregaron 2 campos que faltaban de la visibilidad de la publicacion

--- scriptCargaInicial.sql
+++ scriptCargaInicial.sql
@@ -7,4 +7,5 @@ select  distinct Publ_Cli_Dni  ,publ_cli_nombre,Publ_Cli_Apeliido,Publ_Cli_Fecha
 select distinct Publ_Empresa_cuit,Publ_Empresa_Razon_Social,Publ_Empresa_Dom_Calle ,Publ_Empresa_Nro_Calle ,Publ_Empresa_Piso,Publ_Empresa_Depto,Publ_Empresa_Cod_Postal,Publ_Empresa_Mail,Publ_Empresa_Fecha_Creacion from gd_esquema.Maestra where Publ_Empresa_cuit is not null order by Publ_Empresa_cuit
 
 --visibilidades (gratis,plata,oro,bronce,platino)
-select distinct Publicacion_Visibilidad_Cod,Publicacion_Visibilidad_desc from gd_esquema.Maestra  where Publicacion_Visibilidad_Cod is not null
+select distinct Publicacion_Visibilidad_Cod,Publicacion_Visibilidad_desc,Publicacion_Visibilidad_Precio,Publicacion_Visibilidad_Porcentaje  from gd_esquema.Maestra  where Publicacion_Visibilidad_Cod is not null
+
