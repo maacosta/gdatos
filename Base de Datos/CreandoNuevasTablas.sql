e529bb29a161e0d0807bc43015bc8ed7db1beba8
katerim <katerimserranoguzman@gmail.com>
2016-06-05 06:53:49 +0200

    porsolucionarIdentity en Publicacion

--- Base de Datos/CreandoNuevasTablas.sql
+++ Base de Datos/CreandoNuevasTablas.sql
@@ -1,8 +1,4 @@
-
-drop table  dbo.#TC
-drop table dbo.#te
-go
- 
+/* 
 create table #TC(TC_Cli_Dni numeric (18, 0),
 	TC_Cli_Apellido nvarchar (255),
 	TC_Cli_Nombre nvarchar (255),
@@ -100,3 +96,71 @@ select 	Id,te_Empresa_Cuit,te_Empresa_Razon_Social,te_Empresa_Fecha_Creacion fro
 where te_Empresa_Mail = Mail and te_Empresa_Dom_Calle = Calle and te_Empresa_Nro_Calle = Numero
 and te_Empresa_Piso = Piso and te_Empresa_Depto = Depto and te_Empresa_Cod_Postal = CodigoPostal
 order by Id 
+ 
+
+ -------------------------------------------------------------------------------
+ -----------------------FormaPago---------------------------------------------------------
+ 
+insert into gd_esquema.FormaPago(Descripcion)
+select distinct Forma_Pago_Desc from gd_esquema.Maestra where Forma_Pago_Desc is not null
+go 
+--insert into gd_esquema.Localidad (Id,Nombre )
+----------------------------------------------------------------------------------------------
+------------------------Rubro-------------------------------------------------------------
+
+IF EXISTS (SELECT * FROM sys.tables  WHERE	name = 'tr')
+DROP TABLE dbo.#tr
+GO
+create table #tr(trDescLarga nvarchar(255),
+     trCodigo numeric (18,0) identity (100,1) NOT NULL,
+	trDescCorta nvarchar(50) )
+ go
+ insert into dbo.#tr (trDescLarga,trDescCorta)
+select distinct Publicacion_Rubro_Descripcion, dbo.descripcionCorta(Publicacion_Rubro_Descripcion)  
+from gd_esquema.Maestra where Publicacion_Rubro_Descripcion is not null order by 2
+go 
+insert into gd_esquema.Rubro (DescLarga,Codigo,DescCorta)
+select trDescLarga,trCodigo,trDescCorta from dbo.#tr
+go
+
+------------------------------------------------------------------------------------
+------------------Visivilidad-------------------------------------------------------
+
+insert into gd_esquema.Visibilidad (Descripcion,Precio,Porcentaje,PorcentajeEnvio)
+select distinct Publicacion_Visibilidad_desc,Publicacion_Visibilidad_Precio,Publicacion_Visibilidad_Porcentaje,dbo.porcentajeEnvio(Publicacion_Visibilidad_Porcentaje)
+from gd_esquema.Maestra  where Publicacion_Visibilidad_Cod is not null  
+go
+
+*/
+
+------------------------------------------------------------------------------------
+-------------------------Publicacion-------------------------------------------------
+
+
+insert into gd_esquema.Publicacion (Codigo,TipoPublicacion,Estado,Descripcion,
+	Stock,FechaInicio,FechaVencimiento,Precio,IdUsuario1,IdRubro,IdVisibilidad,IdUsuario)
+
+select Publicacion_Cod,Publicacion_Tipo,Publicacion_Estado, Publicacion_Descripcion,Publicacion_Stock,
+ Publicacion_Fecha,Publicacion_Fecha_Venc,Publicacion_Precio,u.Id, rubroId, viId, usuPublico
+from usuarioPublicaron() as up
+left join Usu_Cli_Emp()as u
+on u.dni = up.Cli_Dni
+
+ 
+
+/*left join dbo.PublicacionVisibilidad  
+on Publicacion_Visibilidad_Cod = PublicacionVisibilidad_Cod
+left join dbo.PublicacionTipo
+on Publicacion_Tipo = PublicacionTipo_Descripcion
+left join dbo.PublicacionRubro
+on Publicacion_Rubro_Descripcion = PublicacionRubro_Descripcion
+left join (select Usuario_Id,Empresa_Razon_Social,Empresa_Cuit,Cliente_Dni from dbo.Usuario
+                 left join dbo.Empresa on Usuario_Id = Empresa_Id
+                 left join dbo.Cliente on Usuario_Id = Cliente_Id) as T
+on  (Publ_Empresa_Razon_Social = T.Empresa_Razon_Social and Publ_Empresa_Cuit = T.Empresa_Cuit) or (Publ_Cli_Dni = T.Cliente_Dni)
+
+
+select * from gd_esquema.Rubro
+select * from gd_esquema.Visibilidad
+*/
+
