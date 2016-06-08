1abe5af0f425900a3008952d336c9a2f7dde9f99
katerim <katerimserranoguzman@gmail.com>
2016-06-08 23:23:47 +0200

    seQuitoId y se modifico Scrip modificacionDtablas

--- Base de Datos/script_mig_05_publicacion.sql
+++ Base de Datos/script_mig_05_publicacion.sql
@@ -1,25 +1,17 @@
-create table #tpu(tpId int identity  not null,
-	tpTipoPublicacion nvarchar(255), 
-	tpEstado char (1) ,
-	tpDescripcion nvarchar(255),
-	tpStock numeric (18, 0), 
-	tpFechaInicio datetime,
-	tpFechaVencimiento datetime ,
-	tpPrecio numeric (18, 2),
-	tpIdUsuario1 int ,
-	tpIdRubro int,
-	tpIdVisibilidad int ,
-	tpIdUsuario int NOT NULL) 
-go
- 
-insert into dbo.#tpu(tpTipoPublicacion,tpEstado,tpDescripcion,tpStock,tpFechaInicio,tpFechaVencimiento,tpPrecio,tpIdUsuario1,tpIdRubro,tpIdVisibilidad,tpIdUsuario)
-select Publicacion_Tipo, dbo.publicacionEstado(Publicacion_Estado), Publicacion_Descripcion,Publicacion_Stock,Publicacion_Fecha,Publicacion_Fecha_Venc,Publicacion_Precio,u.Id, rubroId, viId, usuPublico
-from usuarioPublicaron() as up
+select distinct Publicacion_Cod,Publ_Cli_Dni, Publ_Empresa_Razon_Social,Publ_Empresa_Cuit,Publicacion_Tipo,Publicacion_Estado, Publicacion_Descripcion,Publicacion_Stock,Publicacion_Fecha,Publicacion_Fecha_Venc,Publicacion_Precio,Publicacion_Visibilidad_Desc,Publicacion_Rubro_Descripcion 
+into #Tpu
+from gd_esquema.Maestra
+group by Publicacion_Cod,Publ_Cli_Dni, Publ_Empresa_Razon_Social,Publ_Empresa_Cuit,Publicacion_Tipo,Publicacion_Estado, Publicacion_Descripcion,Publicacion_Stock,Publicacion_Fecha,Publicacion_Fecha_Venc,Publicacion_Precio,Publicacion_Visibilidad_Desc,Publicacion_Rubro_Descripcion
+order by Publicacion_Cod
+
+insert into gd_esquema.Publicacion(TipoPublicacion,Estado,Descripcion,Stock,FechaInicio,FechaVencimiento,Precio,IdUsuario,IdRubro,IdVisibilidad) 
+
+select Publicacion_Tipo, dbo.publicacionEstado(Publicacion_Estado), Publicacion_Descripcion,Publicacion_Stock,Publicacion_Fecha,Publicacion_Fecha_Venc,Publicacion_Precio,u.Id, rubro.Id, visibilidad.Id
+from dbo.#Tpu
 left join Usu_Cli_Emp()as u
-on u.dni = up.Cli_Dni 
+on (Dni = Publ_Cli_Dni) or ((RazonSocial = Publ_Empresa_Razon_Social ) and (Publ_Empresa_Cuit = Cuit))
+left join gd_esquema.Visibilidad
+on Publicacion_Visibilidad_Desc = Descripcion
+left join gd_esquema.Rubro
+on Publicacion_Rubro_Descripcion = DescLarga
 go 
-
-insert into gd_esquema.Publicacion(Id,TipoPublicacion,Estado,Descripcion,Stock,FechaInicio,FechaVencimiento,Precio,IdUsuario1,IdRubro,IdVisibilidad,IdUsuario) 
-select * from dbo.#tpu
-go
-
