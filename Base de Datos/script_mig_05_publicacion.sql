c88b44f1132ec093d5960686b9178dc81260dd1a
maacosta <manuel.alejandro.acosta@gmail.com>
2016-06-09 00:35:47 -0300

    se corrige script de migracion de publicacion

--- Base de Datos/script_mig_05_publicacion.sql
+++ Base de Datos/script_mig_05_publicacion.sql
@@ -23,13 +23,22 @@ where [Publicacion_Cod] is not null
 insert into gd_esquema.Publicacion 
 	(Codigo, TipoPublicacion, Estado, Descripcion, Stock, FechaInicio, FechaVencimiento, Precio, IdRubro, IdVisibilidad, IdUsuario)
 select 
-	t.codigo, t.tipoPublicacion, t.estado, t.descripcion, t.stock, t.fechaInicio, t.fechaVencimiento, t.precio, r.Id, v.Id, u.Id
+	t.codigo, 
+	t.tipoPublicacion, 
+	t.estado, 
+	t.descripcion, 
+	t.stock, 
+	t.fechaInicio, 
+	t.fechaVencimiento, 
+	t.precio, 
+	r.Id, 
+	v.Id, 
+	case when c.dni is null then e.IdUsuario else c.IdUsuario end idUsuario
 from #TPublicacion t
-	inner join gd_esquema.Rubro r on r.DescCorta = t.descripcion
+	inner join gd_esquema.Rubro r on r.DescCorta = t.rubroDesc
 	inner join gd_esquema.Visibilidad v on v.codigo = t.visibilidadCodigo
-	inner join gd_esquema.Cliente c on c.dni = t.clienteDni
-	inner join gd_esquema.Empresa e on e.cuit = t.empresaCuit
-	inner join gd_esquema.Usuario u on u.id = c.idusuario
+	left join gd_esquema.Cliente c on t.clienteDni = c.dni
+	left join gd_esquema.Empresa e on t.empresaCuit = e.cuit
 
 go
 
