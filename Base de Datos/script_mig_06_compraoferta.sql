176f44b6f479402b138483fc291c35293ffaad7a
maacosta <manuel.alejandro.acosta@gmail.com>
2016-06-10 11:10:43 -0300

    script mig compra oferta

--- Base de Datos/script_mig_06_compraoferta.sql
+++ Base de Datos/script_mig_06_compraoferta.sql
@@ -1,19 +1,34 @@
-select distinct Publicacion_Cod,Publ_Cli_Dni,Publ_Empresa_Cuit,Publ_Empresa_Razon_Social,Cli_Dni,Publicacion_Tipo,Compra_Fecha as fecha,Compra_Cantidad ,Oferta_Monto
- into #TCompOferta
- from gd_esquema.Maestra where (Publicacion_Tipo = 'Compra Inmediata') and Compra_Fecha is not null
- union
-select distinct Publicacion_Cod,Publ_Cli_Dni,Publ_Empresa_Cuit,Publ_Empresa_Razon_Social,Cli_Dni,Publicacion_Tipo,Oferta_Fecha as fecha ,Compra_Cantidad ,Oferta_Monto
- from gd_esquema.Maestra where Publicacion_Tipo = 'Subasta' and Oferta_Fecha is not null
- go
- 
- insert into gd_esquema .CompraOferta (IdPublicacion,IdUsuario,IdUsuario1,Tipo,Fecha,Cantidad,Monto)
- select Codigo,u.Id,otraU.Id,dbo.publicacionTipo(Publicacion_Tipo),fecha,Compra_Cantidad ,Oferta_Monto 
- from dbo.#TCompOferta as c
- left join Usu_Cli_Emp() as u
- on (Publ_Cli_Dni = u.Dni ) or (Publ_Empresa_Razon_Social = u.RazonSocial and Publ_Empresa_Cuit = u.Cuit)
- left join gd_esquema.Publicacion
- on Codigo = Publicacion_Cod 
- left join Usu_Cli_Emp() as oTraU
- on Publ_Cli_Dni = OtraU.Dni 
- 
- go 
+IF OBJECT_ID('tempdb..#TCompraOferta') IS NOT NULL DROP TABLE #TCompraOferta
+GO
+
+select co.* into #TCompraOferta
+from (
+select distinct 
+	Publicacion_Cod codigo,
+	Cli_Dni dni,
+	'C' tipo,
+	Compra_Fecha fecha,
+	Compra_Cantidad cantidad,
+	Publicacion_Precio monto
+from gd_esquema.Maestra 
+where Publicacion_Cod is not null and Compra_Fecha is not null
+union
+select distinct 
+	Publicacion_Cod codigo,
+	Cli_Dni dni,
+	'O' tipo,
+	Oferta_Fecha fecha,
+	1 cantidad,
+	Oferta_Monto monto
+from gd_esquema.Maestra 
+where Publicacion_Cod is not null and Oferta_Fecha is not null
+) co
+go
+
+insert into gd_esquema.CompraOferta 
+	(IdPublicacion, IdUsuario, Tipo, Fecha, Cantidad, Monto)
+select p.Id, c.IdUsuario, tipo, fecha, cantidad, monto
+from #TCompraOferta co
+	inner join gd_esquema.Publicacion p on co.codigo = p.Codigo
+	inner join gd_esquema.Cliente c on co.dni = c.Dni
+go 
