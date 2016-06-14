9e724513cb8bb082e011ea94072deaa740c76244
maxiqutn <maqarg@gmail.com>
2016-06-14 19:57:49 -0300

    ser verifica que no este dado de baja el usuario

--- Base de Datos/sp_usuario_get_logindata.sql
+++ Base de Datos/sp_usuario_get_logindata.sql
@@ -5,7 +5,9 @@ CREATE PROCEDURE sp_usuario_Get_logindata  (@username varchar(50))
 
 AS
 	BEGIN		
-        select Username,PassSalt,intentos from gd_esquema.Usuario	 where Username=@username
+        select Username,PassSalt,intentos from gd_esquema.Usuario	 where Username=@username and FechaBaja is not null
 	END
 GO
+
+
 
