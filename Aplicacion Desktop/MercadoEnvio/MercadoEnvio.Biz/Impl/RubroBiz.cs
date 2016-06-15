e121c2d8f3322c33b53d33608bd6feda7a44613d
maacosta <manuel.alejandro.acosta@gmail.com>
2016-06-14 21:20:48 -0300

    Se completa logica de rubro

---
+++ Aplicacion Desktop/MercadoEnvio/MercadoEnvio.Biz/Impl/RubroBiz.cs
@@ -0,0 +1,25 @@
+﻿using MercadoEnvio.Common.Entity;
+using MercadoEnvio.Dal.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MercadoEnvio.Biz.Impl
+{
+    public class RubroBiz
+    {
+        private RubroDal _rubroDal;
+
+        public RubroBiz()
+        {
+            this._rubroDal = new RubroDal();
+        }
+
+        public List<Rubro> GetByDesc(string texto)
+        {
+            return this._rubroDal.GetByDesc(texto);
+        }
+    }
+}
---
+++ Aplicacion Desktop/MercadoEnvio/MercadoEnvio.Dal/MercadoEnvio.Dal/Impl/RubroDal.cs
@@ -0,0 +1,23 @@
+﻿using MercadoEnvio.Common.Entity;
+using MercadoEnvio.Dal.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+
+namespace MercadoEnvio.Dal.Impl
+{
+    public class RubroDal : BaseDal
+    {
+        public List<Rubro> GetByDesc(string texto)
+        {
+            object[] param = new object[] { null, null, texto, texto };
+
+            var data = this._db.ExecuteSprocAccessor<Rubro>("gd_esquema.sp_rubro_getRubroFiltros", param).ToList();
+
+            return data;
+        }
+    }
+}
