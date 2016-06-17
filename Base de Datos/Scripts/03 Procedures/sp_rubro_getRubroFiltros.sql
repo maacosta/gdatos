36f0ee186c9776e6b29c9d13655cd43aedd9842f
efzinetti <efzinetti@gmail.com>
2016-06-17 12:22:55 -0300

    parámetros
    pongo solo 1 parámetro de entrada, que se va a comparar con descCorta y
    descLarga

--- Base de Datos/Scripts/03 Procedures/sp_rubro_getRubroFiltros.sql
+++ Base de Datos/Scripts/03 Procedures/sp_rubro_getRubroFiltros.sql
@@ -1,40 +1,17 @@
 IF (OBJECT_ID('gd_esquema.sp_rubro_getRubroFiltros') IS NOT NULL)
-drop PROCEDURE gd_esquema.sp_rubro_getRubroFiltros
-go
+  Drop Procedure gd_esquema.sp_rubro_getRubroFiltros
+Go
 
-CREATE PROCEDURE gd_esquema.sp_rubro_getRubroFiltros (@id int, @codigo numeric(18,0), @desccorta nvarchar(50), @desclarga nvarchar(255))
+CREATE PROCEDURE gd_esquema.sp_rubro_getRubroFiltros (@Descripcion nvarchar(255))
 AS
 
 BEGIN
 
-declare @query nvarchar(1024)
-
-set @query = 'select id, codigo, DescCorta, DescLarga from gd_esquema.Rubro'
-
-set @query = @query + ' WHERE 1=1'
-
-if (@id is not null) set @query = @query + ' AND ID=' + convert(char(5),@id)
-if (@codigo is not null) set @query = @query + ' AND CODIGO=' + convert(char(10),@codigo)
-if (@desccorta is not null) set @query = @query + ' AND DescCorta=''' + @desccorta + ''''
-if (@desclarga is not null) set @query = @query + ' AND DescLarga=''' + @desclarga + ''''
-
-exec (@query)
+SELECT Id, Codigo, DescCorta, DescLarga
+  FROM gd_esquema.Rubro
+ WHERE @Descripcion IS NULL
+    OR DescCorta LIKE '%' + @Descripcion + '%'
+    OR DescLarga LIKE '%' + @Descripcion + '%'
 
 END
 GO
-
-/*
-exec sp_rubro_getRubroFiltros null, null, null, null
-go
-
-exec sp_rubro_getRubroFiltros null, null, 'Soportes', null
-go
-
-exec sp_rubro_getRubroFiltros null, null, 'Computación', null
-go
-
-exec sp_rubro_getRubroFiltros null, '105', null, null
-go
-
-exec sp_rubro_getRubroFiltros 24, null, null, null
-go*/
