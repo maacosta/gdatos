57d8d71183d7e2b3319f2ffe19d118b36897cff7
maacosta <manuel.alejandro.acosta@gmail.com>
2016-06-26 18:27:38 -0300

    Correccion de fecha en getFinalizadaFiltros

--- Base de Datos/Scripts/04 Procedures/sp_publicacion_getFinalizadaFiltros.sql
+++ Base de Datos/Scripts/04 Procedures/sp_publicacion_getFinalizadaFiltros.sql
@@ -2,23 +2,41 @@ IF (OBJECT_ID('LOS_DE_ADELANTE.sp_publicacion_getFinalizadaFiltros') IS NOT NULL
 	drop PROCEDURE LOS_DE_ADELANTE.sp_publicacion_getFinalizadaFiltros
 go
 
-create procedure LOS_DE_ADELANTE.sp_publicacion_getFinalizadaFiltros (@usermane nvarchar(50), @fechaSistema date) as
+CREATE PROCEDURE LOS_DE_ADELANTE.sp_publicacion_getFinalizadaFiltros
+(
+	@username nvarchar(50), 
+	@fechaSistema datetime
+)
+AS
+BEGIN		
 
-	begin
+	select
+		p.Id, 
+		p.Codigo, 
+		p.TipoPublicacion, 
+		p.Estado, 
+		p.Descripcion, 
+		p.Stock, 
+		p.FechaInicio, 
+		p.FechaVencimiento, 
+		p.Precio, 
+		p.Costo, 
+		p.PermitirPreguntas, 
+		p.IncluirEnvio, 
+		p.IdRubro, 
+		r.Codigo CodigoRubro, 
+		p.IdVisibilidad, 
+		v.Codigo CodigoVisibilidad, 
+		u.Username Usuario
+	from LOS_DE_ADELANTE.Publicacion p
+		inner join LOS_DE_ADELANTE.Rubro r on r.Id = p.IdRubro
+		inner join LOS_DE_ADELANTE.Visibilidad v on v.Id = p.IdVisibilidad
+		inner join LOS_DE_ADELANTE.Usuario u on u.Id = p.IdUsuario
+	where CONVERT(date, p.FechaVencimiento) < CONVERT(date, @fechaSistema)
+		and u.Username = @username
+		and p.Estado = 'A'
 
-	select pub.Id, pub.Codigo, TipoPublicacion, Estado, pub.Descripcion, Stock, FechaInicio, FechaVencimiento, pub.Precio,
-		   IdRubro, rub.Codigo, IdVisibilidad, vis.Codigo, usr.Username
-	  from LOS_DE_ADELANTE.Publicacion pub,
-		   LOS_DE_ADELANTE.Usuario usr,
-		   LOS_DE_ADELANTE.Rubro rub,
-		   LOS_DE_ADELANTE.Visibilidad vis
-	 where pub.IdUsuario = usr.Id
-	   and pub.IdRubro = rub.Id
-	   and pub.IdVisibilidad = vis.Id
-	   and usr.Username = @usermane
-	   and pub.Estado = 'A'
-	   and pub.FechaVencimiento < @fechaSistema
-
-	end
-
+END
 go
+
+
